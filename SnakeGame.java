5a2789e717c68ccd7e14150ed6610b9d35004715
Ömer Uman <159270464+uumannn@users.noreply.github.com>
2025-03-13 12:10:48 +0300

    Add files via upload

---
+++ SnakeGame.java
@@ -0,0 +1,23 @@
+public class SnakeGame {
+    public static void main(String[] args) {
+
+
+        new GameFrame();
+
+  // oyun bitince tekrar başlama butonu?
+        //icinde cıkmasın baloncuk eklenebilir feautrue
+
+        //00:03 Creating a snake game using Java
+        //03:36 Setting up the game frame and necessary methods
+        //08:15 Initializing game variables and arrays for the snake game.
+        //12:00 Initializing game panel with timer and random class instance
+        //15:39 Creating a grid for visualization
+        //19:04 Creating the move method for the snake game.
+        //22:08 Shifting coordinates and changing snake direction
+        //25:28 Setting the color and implementing movement for the snake's body
+        //29:06 Checking for snake collision with borders and controlling the snake's movement.
+        //32:58 Updating snake position and increasing body parts
+        //36:42 Placing game over and score display on the screen
+        //39:59 Using random colors to create a multicolored snake
+    }
+}
